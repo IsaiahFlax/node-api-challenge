2044bc07635a9a2450315b8faa809388e52fd63d
IsaiahFlax <flax.isaiah@gmail.com>
2020-05-15 20:42:48 -0600

    get projects by id

--- crud/projectscrud.js
+++ crud/projectscrud.js
@@ -14,4 +14,28 @@ router.get("/", (req, res) => {
       .catch((err) => {})
   })
 
+  router.get("/:id", (req, res) => {
+    // do your magic!
+    const { id } = req.params
+    projectModel.get(id)
+      .then((projects) => {
+          if (projects) {
+            res.status(200).json({ message: `Project number ${projects.id}`, projects })
+          } else {
+            res.status(400).json({ message: 'There is no project with this id'})
+      }})
+      .catch((err) => {})
+  })
+
+  router.get("/:id/actions", (req, res) => {
+    // do your magic!
+    const { id } = req.params
+    projectModel.getProjectActions()
+      .then((projects) => {
+          if (req.params)
+        res.status(200).json({ message: "All projects on server", projects })
+      })
+      .catch((err) => {})
+  })
+
   module.exports = router
